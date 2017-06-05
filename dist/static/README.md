e9c96ecda78885745be47ed482bf5f7e49ec68de
zhaoxiaoyang <zhaoxiaoyang@vread.cn>
2017-06-05 23:47:50 +0800

    data数据更新

---
+++ dist/static/README.md
@@ -0,0 +1,12 @@
+# requireJs-underscoreJs
+## 目的 
+1.requirejs+underscorejs构建一个简单h5项目
+
+2.用node做服务器
+
+3.构建好的项目要按需打包文件
+
+## 说明 
+### lesson1 分支
+搭建项目
+
--- dist/static/src/js/config.js
+++ dist/static/src/js/config.js
@@ -1,1 +1,24 @@
-require.config({paths:{underscore:"lib/underscore",zepto:"lib/zepto",swiper:"lib/swiper",text:"text",header:"header",banner:"banner","index-list":"index-list","footer-nav":"footer-nav"},shim:{zepto:{exports:"$"},underscore:{exports:"_"}}});
+require.config({
+	//指定目录
+	// baseUrl:'../js',//baseUrl是基於.html也main的相對路徑
+	//配置各种库的基本信息
+	paths:{
+		'underscore':'lib/underscore',
+		'zepto':'lib/zepto',
+		'swiper':'lib/swiper',
+		'text':'text',
+		'header':'header',
+		'banner':'banner',
+		'index-list':'index-list',
+		'footer-nav':'footer-nav'
+	},
+	//配置输出
+	shim:{
+		zepto:{
+			exports:"$"
+		},
+		underscore:{
+			exports:"_"
+		}
+	}
+})
